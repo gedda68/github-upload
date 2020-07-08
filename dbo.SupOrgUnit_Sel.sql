e1075d48964382ba5371f2d2c33584d29a4cf2ce
gedda <44280209+gedda68@users.noreply.github.com>
2020-07-08 12:44:42 +1000

    Removed TBA
    https://hcms.atlassian.net/browse/PMO-12571

--- dbo.SupOrgUnit_Sel.sql
+++ dbo.SupOrgUnit_Sel.sql
@@ -8,7 +8,7 @@ as
 select
 isnull(case when NewOrgUnit not like 'NEW%' then T106F005_ORGANISATION_UNIT_NO
 else (T106F005_ORGANISATION_UNIT_NO+T101F005_POSITION_NO) end,'') as [Back to ToC]
-,'TBA' as Group2
+,'' as Group2
 ,'' as Group3
 ,'' as Group4
 ,'' as Group5
